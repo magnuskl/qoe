42601b7e1744269720951929afb768a58e974e1e
Magnus Klausen <magnus.klausen@gmail.com>
2021-03-01 20:38:27 +0100

    added quotation of user agent strings

--- scripts/submit.php
+++ scripts/submit.php
@@ -5,30 +5,30 @@ $data = "";
 
 // Insert header before first entry.
 if (!file_exists($file)) {
-    $data = "candidate_number"  . "," . 
-            "age"               . "," .
-            "user_agent_simple" . "," .
-            "user_agent_full"   . "," .
-            "proficiency"       . "," .
-            "experiment_id"     . "," .
-            "timestamp"         . "," .
-            "first_variant"     . "," .
-            "second_variant"    . "," .
-            "selected_variant"  . "\n";
+    $data = "candidate_number,"  . 
+            "age,"               .
+            "user_agent_simple," .
+            "user_agent_full,"   .
+            "proficiency,"       .
+            "experiment_id,"     .
+            "timestamp,"         .        
+            "first_variant,"     .
+            "second_variant,"    .
+            "selected_variant\n";
 }
 
 // Prepend metadata to each entry.
 $entries = explode(PHP_EOL, $_GET["data"]);
 foreach ($entries as $entry) {
     if (!empty($entry)) {
-        $data .= $_GET["cand"]          . ',' .
-                 $_GET["age"]           . ',' .
-                 $_GET["agent_simple"]  . ',' .
-                 $_GET["agent_full"]    . ',' .
-                 $_GET["prof"]          . ',' .
-                 $_GET["exp"]           . ',' .
-                 $_GET["time"]          . ',' .
-                 $entry                 . "\n";
+        $data .= $_GET["cand"]               . ','  .
+                 $_GET["age"]                . ','  .
+                 '"' . $_GET["agent_simple"] . '",' .
+                 '"' . $_GET["agent_full"]   . '",'  .
+                 $_GET["prof"]               . ','  .
+                 $_GET["exp"]                . ','  .
+                 $_GET["time"]               . ','  .
+                 $entry                      . "\n";
     }
 }
 
